0fb3974369856fc53b21f3d425afd3d3b84d338d
Kamigaku <kamigaku@gmail.com>
2021-09-16 19:49:35 +0200

    commit

--- migrations/migration_2.py
+++ migrations/migration_2.py
@@ -7,13 +7,13 @@ from discordClient.model import CharactersOwnership
 migrator = SqliteMigrator(dbContext.DbContext().sqliteConnection)
 
 migrate(
-    # migrator.add_column("character", "url_link", TextField(default="")),
-    # migrator.add_column("charactersownership", "is_sold", BooleanField(default=False)),
-    # migrator.add_column("charactersownership", "dropped_by", IntegerField(default="")),
+    migrator.add_column("character", "url_link", TextField(default="")),
+    migrator.add_column("charactersownership", "is_sold", BooleanField(default=False)),
+    migrator.add_column("charactersownership", "dropped_by", IntegerField(default="")),
     migrator.drop_column("trade", "confirmation_code")
 )
 
-# characterownerships = CharactersOwnership.select()
-# for ownership in characterownerships:
-#     ownership.dropped_by = ownership.discord_user_id
-#     ownership.save()
+characterownerships = CharactersOwnership.select()
+for ownership in characterownerships:
+    ownership.dropped_by = ownership.discord_user_id
+    ownership.save()
