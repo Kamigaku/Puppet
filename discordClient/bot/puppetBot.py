ce55103b23d41dc2a87d8d297d957fcf70b1038e
Kamigaku <kamigaku@gmail.com>
2021-10-17 17:38:41 +0200

    Removed the debug guild

--- discordClient/bot/puppetBot.py
+++ discordClient/bot/puppetBot.py
@@ -21,7 +21,7 @@ class PuppetBot(Bot):
         intents.reactions = True
         super().__init__(command_prefix=commands_prefix, intents=intents,
                          self_bot=True)
-        self.slash = SlashCommand(self, sync_commands=True, debug_guild=877098506211442719)
+        self.slash = SlashCommand(self, sync_commands=True)
         self.reaction_listeners = {}
         self.delete_listeners = {}
 
